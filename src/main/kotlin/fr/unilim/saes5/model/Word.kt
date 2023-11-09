69beec21435f73adca46f3b28d4e12dc7733382c
nelpats <emalf.pro@gmail.com>
2023-11-09 11:08:38 +0100

    Added equals method to Word

--- src/main/kotlin/fr/unilim/saes5/model/Word.kt
+++ src/main/kotlin/fr/unilim/saes5/model/Word.kt
@@ -8,18 +8,28 @@ class Word(var token: String?) {
     var antonyms: Set<Word>? = HashSet()
     var context: Context? = null
 
+
+
     constructor() : this(null)
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (other == null || this::class != other::class) return false
+        if (javaClass != other?.javaClass) return false
 
         other as Word
 
-        return token == other.token
+        if (token != other.token) return false
+        if (synonyms != other.synonyms) return false
+        if (related != other.related) return false
+        if (antonyms != other.antonyms) return false
+        if (context != other.context) return false
+
+        return true
     }
 
-    override fun hashCode(): Int {
-        return token?.hashCode() ?: 0
+
+    override fun toString(): String {
+        return "Word(token=$token)"
     }
+
 }
