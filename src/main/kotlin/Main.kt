cfba63c4a5dff4beba4ac22abd2eb976dd82d316
nelpats <emalf.pro@gmail.com>
2023-11-16 09:00:21 +0100

    Created a little console exmaple in Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,22 @@
 package fr.unilim.saes5
 
-import fr.unilim.saes5.model.reader.SingleFileReader
+import fr.unilim.saes5.model.WordAnalytics
+import fr.unilim.saes5.model.reader.JavaFileReader
 
 fun main(args: Array<String>) {
-    val words = SingleFileReader().readOne("example/Gameplay.java")
+    val exampleFilePath = "example/Gameplay.java"
+    val words = JavaFileReader().readOne(exampleFilePath)
+    println(words.size.toString() + " mots ont été trouvés :")
 
-    for (word in words) {
-        println(word)
+    println("---------------------------------------------")
+
+    val analytics = WordAnalytics()
+    val wordRank = analytics.wordRank(words)
+
+    println("Voici la liste des mots trouvés par occurence")
+    for ((word, count) in wordRank) {
+        println("$word  $count")
     }
+
+
 }
