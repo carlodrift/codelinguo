c8be8074549e0dfdeb2f277e4ee2d9c9eb6e946d
lucaspialleport <lucaspialleport@icloud.com>
2023-12-07 14:14:08 +0100

    Update Resizable setting

--- src/main/kotlin/fr/unilim/saes5/Main.kt
+++ src/main/kotlin/fr/unilim/saes5/Main.kt
@@ -2,10 +2,19 @@ package fr.unilim.saes5
 
 import fr.unilim.saes5.view.MainView
 import fr.unilim.saes5.view.Styles
+import javafx.stage.Stage
 import tornadofx.App
 import tornadofx.launch
 
-class MainApp : App(MainView::class, Styles::class)
+class MainApp : App(MainView::class, Styles::class) {
+    override fun start(stage: Stage) {
+        with(stage) {
+            isResizable = false
+            isMaximized = false
+        }
+        super.start(stage)
+    }
+}
 
 fun main(args: Array<String>) {
     launch<MainApp>(args)
