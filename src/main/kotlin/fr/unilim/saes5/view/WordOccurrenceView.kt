68c23070699136839edad1e57758eebf67c818c5
Clément Raynaud <clement.raynaud2@etu.unilim.fr>
2023-12-07 17:23:34 +0100

    fix: string word occurence view

--- src/main/kotlin/fr/unilim/saes5/view/WordOccurrenceView.kt
+++ src/main/kotlin/fr/unilim/saes5/view/WordOccurrenceView.kt
@@ -7,7 +7,7 @@ class WordOccurrenceView(private val wordRank: Map<String, Int>) : Fragment() {
     override val root = vbox {
         val wordRankList = FXCollections.observableArrayList(wordRank.entries.toList())
         tableview(wordRankList) {
-            readonlyColumn("Word", Map.Entry<String, Int>::key)
+            readonlyColumn("Mot", Map.Entry<String, Int>::key)
             readonlyColumn("Occurrences", Map.Entry<String, Int>::value)
             columnResizePolicy = SmartResize.POLICY
         }
