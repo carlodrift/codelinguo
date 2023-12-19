56cd02ee4a9be8f4d654a48c9b8c27f466584975
nelpats <emalf.pro@gmail.com>
2023-12-19 10:00:46 +0100

    Implemented the glossaryRatio method

--- src/main/kotlin/fr/unilim/saes5/service/WordAnalyticsService.kt
+++ src/main/kotlin/fr/unilim/saes5/service/WordAnalyticsService.kt
@@ -1,5 +1,6 @@
 package fr.unilim.saes5.service
 
+import fr.unilim.saes5.model.Glossary
 import fr.unilim.saes5.model.Word
 
 class WordAnalyticsService {
@@ -24,4 +25,14 @@ class WordAnalyticsService {
         }
         return wordCount.entries.sortedByDescending { it.value }.associate { it.toPair() }
     }
+
+    fun glossaryRatio(words: List<Word?>, glossary: Glossary): Float {
+        if (words.isNullOrEmpty()) {
+            return 0.0f
+        }
+        val glossaryWords = glossary.words.map { it.token }.toSet()
+        val count = words.count { it?.token in glossaryWords }
+        return count.toFloat() / words.size
+    }
+
 }
