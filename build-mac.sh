98afe68cf24db97e58abc15af7caea48d63a6c16
Clément Raynaud <clement.raynaud2@etu.unilim.fr>
2024-01-11 10:11:59 +0100

    Rebranding CodeLinguo

--- build-mac.sh
+++ build-mac.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ~/IdeaProjects/sae_s5 && mvn clean package && jpackage --input target/ --name SaeS5 --main-jar sae_s5-1.0.jar --main-class fr.unilim.saes5.MainKt --type dmg
+cd ~/IdeaProjects/sae_s5 && mvn clean package && jpackage --input target/ --name CodeLinguo --main-jar codelinguo-1.0.jar --main-class fr.unilim.saes5.MainKt --type dmg
--- build-windows.bat
+++ build-windows.bat
@@ -1,4 +1,4 @@
 @echo off
 mvn clean package
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-jpackage --input target\ --name SaeS5 --main-jar sae_s5-1.0.jar --main-class fr.unilim.saes5.MainKt --type exe
+jpackage --input target\ --name CodeLinguo --main-jar codelinguo-1.0.jar --main-class fr.unilim.saes5.MainKt --type exe
