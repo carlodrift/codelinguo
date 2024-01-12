100e13a405a088215ce1a7918d701703261ba90f
Clément Raynaud <clement.raynaud2@etu.unilim.fr>
2024-01-12 10:37:51 +0100

    Edit Windows release script

--- build-windows.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-mvn clean package
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-jpackage --input target\ --name CodeLinguo --main-jar codelinguo-1.0.jar --main-class fr.unilim.saes5.MainKt --type exe
