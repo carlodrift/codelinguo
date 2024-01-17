062e49cc5863b7ef1d23f4f1ddafa77cdaad97ee
Clément Raynaud <clement.raynaud2@etu.unilim.fr>
2024-01-17 15:42:38 +0100

    Add isProjectNameAvailable in JsonGlossaryDao

--- src/main/kotlin/fr/unilim/saes5/persistence/glossary/JsonGlossaryDao.java
+++ src/main/kotlin/fr/unilim/saes5/persistence/glossary/JsonGlossaryDao.java
@@ -2,7 +2,6 @@ package fr.unilim.saes5.persistence.glossary;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 import fr.unilim.saes5.model.Glossary;
 import fr.unilim.saes5.model.context.Context;
 
@@ -12,7 +11,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,13 +19,13 @@ public class JsonGlossaryDao implements GlossaryDao {
     private static final String CODELINGUO_PROJECTS = ".codelinguo/projects";
     private final Gson gson;
 
-    private File directory;
+    private final File directory;
 
     public JsonGlossaryDao() {
         String userHome = System.getProperty("user.home");
-        this.directory = new File(userHome, CODELINGUO_PROJECTS);
-        if (!directory.exists()) {
-            directory.mkdirs();
+        this.directory = new File(userHome, JsonGlossaryDao.CODELINGUO_PROJECTS);
+        if (!this.directory.exists()) {
+            this.directory.mkdirs();
         }
 
         GsonBuilder gsonBuilder = new GsonBuilder();
@@ -37,7 +35,7 @@ public class JsonGlossaryDao implements GlossaryDao {
 
     @Override
     public void saveProject(Glossary project, String name) {
-        File jsonFile = new File(directory, name);
+        File jsonFile = new File(this.directory, name);
         if (!jsonFile.exists()) {
             try {
                 jsonFile.createNewFile();
@@ -81,5 +79,10 @@ public class JsonGlossaryDao implements GlossaryDao {
                 e.printStackTrace();
             }
         }
+    }
+
+    public boolean isProjectNameAvailable(String projectName) {
+        File projectFile = new File(this.directory, projectName + ".json");
+        return !projectFile.exists();
     }
 }
