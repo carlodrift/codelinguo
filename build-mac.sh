3b88260f4c88a067ba2741d2c6265733d322520e
Clément Raynaud <clement.raynaud2@etu.unilim.fr>
2024-01-18 15:14:29 +0100

    Update build-mac.sh

--- build-mac.sh
+++ build-mac.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ~/IdeaProjects/sae_s5 && mvn clean package && jpackage --input target/ --name CodeLinguo --main-jar codelinguo-1.0.jar --main-class fr.unilim.saes5.MainKt --icon src/main/resources/logo.icns --type dmg
+cd ~/IdeaProjects/sae_s5 && mvn clean package && jpackage --input target/ --name CodeLinguo --main-jar codelinguo-1.1.jar --main-class fr.unilim.saes5.MainKt --icon src/main/resources/logo.icns --type dmg
