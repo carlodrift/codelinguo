3aea2bd383842e9cdb2ede342df22260bc6732af
Clément Raynaud <clement.raynaud2@etu.unilim.fr>
2024-01-29 23:22:31 +0100

    Delete dead code

--- src/main/kotlin/fr/unilim/saes5/model/Glossary.java
+++ src/main/kotlin/fr/unilim/saes5/model/Glossary.java
@@ -21,24 +21,12 @@ public class Glossary {
         this.words = words;
     }
 
-    public Glossary() {
-        this.words = List.of();
-    }
-
     public String getName() {
         return this.name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public void addWord(Word word) {
-        this.words.add(word);
-    }
-
-    public void removeWord(Word word) {
-        this.words.remove(word);
     }
 
     public List<Word> getWords() {
