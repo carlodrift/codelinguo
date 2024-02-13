b7653d3070dde0943cbc5c1efd76f7605e75958e
Clément Raynaud <clement.raynaud2@etu.unilim.fr>
2024-02-13 09:36:27 +0100

    Fix project creation with special characters

--- src/main/kotlin/fr/unilim/codelinguo/view/CreateProjectView.kt
+++ src/main/kotlin/fr/unilim/codelinguo/view/CreateProjectView.kt
@@ -37,15 +37,18 @@ class CreateProjectView : View() {
         button(lang.getMessage("button_validate")) {
             addClass(ViewStyles.projectButton)
             action {
-                if (projectNameTextField.text.trim().isBlank()) {
+                val projectName = projectNameTextField.text.trim()
+                val regex = "^[a-zA-Z0-9]+$".toRegex()
+
+                if (projectName.isBlank() || !projectName.matches(regex)) {
                     alert(
                         Alert.AlertType.WARNING,
                         lang.getMessage("project_name_error_title"),
-                        lang.getMessage("project_name_blank_error_message")
+                        lang.getMessage("project_name_error_message")
                     )
                 } else {
                     primaryStage.close()
-                    find(MainView::class, mapOf(MainView::projectName to projectNameTextField.text.trim())).openWindow()
+                    find(MainView::class, mapOf(MainView::projectName to projectName)).openWindow()
                 }
             }
         }
