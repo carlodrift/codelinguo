e4356d276b525404963209b4dd4a1afaa8520898
Clément Raynaud <clement.raynaud2@etu.unilim.fr>
2024-02-14 15:23:29 +0100

    Fix build scripts 2

--- desktop/build-mac.sh
+++ desktop/build-mac.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-mvn clean package && jpackage --input target/ --name CodeLinguo --main-jar CodeLinguo.jar --main-class fr.unilim.codelinguo.desktop.MainKt --icon src/main/resources/logo/logo.icns --type dmg
+initial_dir=$(pwd)
+
+cd .. && mvn clean install
+
+cd "$initial_dir"
+
+mvn clean package
+jpackage --input target/ --name CodeLinguo --main-jar CodeLinguo.jar --main-class fr.unilim.codelinguo.desktop.MainKt --icon src/main/resources/logo/logo.icns --type dmg
